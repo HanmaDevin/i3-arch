da5ee72fb1926a3427a494b991267f7b734a817a
HanmaDevin <devinbc14@gmail.com>
2025-07-15 17:13:23 +0200

    fixed bluetooth script

--- .config/polybar/bluetooth.sh
+++ .config/polybar/bluetooth.sh
@@ -4,7 +4,7 @@
 status=$(bluetoothctl show | grep "Powered" | awk '{print $2}')
 if [ "$status" = "yes" ]; then
     connected_devices=$(bluetoothctl devices Connected | head -n 1 | wc -l)
-    if [ "$connected_devices" -gt 1 ]; then
+    if [ "$connected_devices" -ge 1 ]; then
         alias=$(bluetoothctl info | grep "Alias" | awk -F ': ' '{print $2}')
         echo " $alias"
     else
